33e9a7b604ca4d2bafdb33a126eef246dfd39e66
ddkangfu <ddkangfu@gmail.com>
2015-02-01 23:57:00 +0800

    modified: 增加了处理post方法的函数，美化了前台模板页面。

--- main/views.py
+++ main/views.py
@@ -6,9 +6,11 @@ from redis_exercises.settings import redis_con
 class  MainView(TemplateView):
     template_name = "main/main.html"
 
-    #def dispatch(self, request, *args, **kwargs):
-    #    self.redis_con = 
-    #    return super(MainView, self).dispatch(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name', None)
+        if name:
+            redis_con.set('name', name)
+        return super(MainView, self).get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         ctx = super(MainView, self).get_context_data(**kwargs)
