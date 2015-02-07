9d1cdc14c946f93951f8a6d37243e4cdefa91cc6
ddkangfu <ddkangfu@gmail.com>
2015-02-07 21:17:35 +0800

    factored: 使用hincrby替换了之前的一系统查询修改操作。

--- main/middlewares.py
+++ main/middlewares.py
@@ -3,7 +3,7 @@
 
 from redis_exercises.settings import redis_con
 import time
-import datetime
+from django.utils import timezone
 
 class StatisticsMiddleware(object):
     def process_request(self, request):
@@ -11,8 +11,5 @@ class StatisticsMiddleware(object):
             redis_con.zadd('users.online', time.time(), request.user.username)
 
         # 进行PV统计，三秒钟内生重复访问无效
-        key = datetime.datetime.now().strftime('%Y%m%d%H%M')
-        print key
-        if not redis_con.hsetnx('users.pv', key, 1):
-            pvs = redis_con.hget('users.pv', key)
-            redis_con.hset('users.pv', key, int(pvs)+1)
+        key = timezone.now().strftime('%Y%m%d%H%M')
+        pvs = redis_con.hincrby('users.pv', key, 1)
