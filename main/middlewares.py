76da68d8b043a7c94e70f85d78c88ff38d165c7d
ddkangfu <ddkangfu@gmail.com>
2015-02-07 21:11:36 +0800

    added: pv统计功能。

--- main/middlewares.py
+++ main/middlewares.py
@@ -3,8 +3,16 @@
 
 from redis_exercises.settings import redis_con
 import time
+import datetime
 
 class StatisticsMiddleware(object):
     def process_request(self, request):
         if request.user.is_authenticated():
             redis_con.zadd('users.online', time.time(), request.user.username)
+
+        # 进行PV统计，三秒钟内生重复访问无效
+        key = datetime.datetime.now().strftime('%Y%m%d%H%M')
+        print key
+        if not redis_con.hsetnx('users.pv', key, 1):
+            pvs = redis_con.hget('users.pv', key)
+            redis_con.hset('users.pv', key, int(pvs)+1)
