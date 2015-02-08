d2fcdc75a4d84360f0dc90e3170ab4a55d4d8331
ddkangfu <ddkangfu@gmail.com>
2015-02-08 21:44:45 +0800

    added: 3秒钟内连续访问不记录pv的限制。

--- main/middlewares.py
+++ main/middlewares.py
@@ -11,5 +11,10 @@ class StatisticsMiddleware(object):
             redis_con.zadd('users.online', time.time(), request.user.username)
 
         # 进行PV统计，三秒钟内生重复访问无效
-        key = timezone.now().strftime('%Y%m%d%H%M')
-        pvs = redis_con.hincrby('users.pv', key, 1)
+        ip = request.META['HTTP_X_FORWARDED_FOR'] if request.META.has_key('HTTP_X_FORWARDED_FOR') else request.META['REMOTE_ADDR']
+        if not redis_con.exists(ip):
+            key = timezone.now().strftime('%Y%m%d%H%M')
+            pvs = redis_con.hincrby('users.pv', key, 1)
+
+            redis_con.set(ip, 0)
+            redis_con.expire(ip, 3)
